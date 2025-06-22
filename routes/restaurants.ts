f9745630693052116e1480393574bb0242783cc6
legendary <145838047+legendryPro@users.noreply.github.com>
2025-06-22 15:54:05 +0530

    step 3 added zod and middlewares

--- routes/restaurants.ts
+++ routes/restaurants.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import { validate } from "../middleware/validate.js";
+import { RestaurantSchema, type Restaurant } from "../schemas/restaurants.js";
 const router = express.Router();
 
 
-router.get('/', async(req,res) =>{
-    res.send("Hello world");
-})
+router.post("/", validate(RestaurantSchema), async (req, res) => {
+  const data = req.body as Restaurant;
+  res.send("Hello world");
+});
 
-export default router; 
+export default router;
